bb2f521498126d7067b0abf7df4a86de48268f8c
HikkusT <luizhenrique98@gmail.com>
2018-02-18 03:27:45 -0300

    Changed the visuals for the player

--- objects/Player.lua
+++ objects/Player.lua
@@ -3,6 +3,7 @@ Player = GameObject:extend()
 function Player:new(area, x, y, opts)
     Player.super.new(self, area, x, y, opts)
     self.r = 25
+    self.width, self.height = 70, 60
     self.collider = self.area.world:newCircleCollider(self.x, self.y, self.r)
     self.collider:setObject(self)
     self.collider:setCollisionClass('Player')
@@ -14,9 +15,41 @@ function Player:new(area, x, y, opts)
     self.maxVelocity = 200
     self.acceleration = 300
 
+    self.maxStamina = 100
+    self.stamina = self.maxStamina
+    self.staminaDecrase = 10
+    self.maxHP = 30
+    self.hp = self.maxHP
+    self.hpDecrase = 1
+    self.minPower = 10
+    self.maxPower = 20
+    self.power = self.minPower
+
     self.timer:every(0.25, function() local ang = random(self.angle + 5*math.pi/6, self.angle + 7*math.pi/6) 
         self.area:AddGameObject('PlayerParticle', self.x + 1.5 * self.r * math.cos(ang), self.y + 1.5 * self.r * math.sin(ang), {r = ang}) 
     end)
+
+    self.polygons = {}
+
+    self.polygons[1] = {
+        self.width/2, 0,
+        -self.width/7, self.height/2,
+        -self.width/2, self.height/4,
+        -7*self.width/20, 0,
+        -self.width/2, -self.height/4,
+        -self.width/7, -self.height/2,
+    }
+
+    self.polygons[2] = {
+        self.width/70, self.height/4,
+        -3*self.width/10, self.height/4,
+        -self.width/6, 0,
+        -3*self.width/10, -self.height/4,
+         self.width/70, -self.height/4,
+        -5*self.width/42, 0,
+    }
+
+    self.polygonColors = {{255, 255, 255, 255}, {255, 255, 255, 255}}
 end
 
 function Player:update(dt)
@@ -24,8 +57,7 @@ function Player:update(dt)
 
     --Handling Input
     self.angle = math.atan2(select(2, love.mouse.getPosition( )) - self.y, select(1, love.mouse.getPosition( )) - self.x)
-    --if input:down('left') then self.angle = self.angle - self.angleVelocity * dt end
-    --if input:down('right') then self.angle = self.angle + self.angleVelocity * dt end
+
     if input:down('up') then self.frontVelocity = math.min(self.frontVelocity + self.acceleration * dt, self.maxVelocity)
     elseif input:down('down') then self.frontVelocity = math.max(self.frontVelocity - self.acceleration * dt, -self.maxVelocity)
     else self.frontVelocity = math.sign(self.frontVelocity) * math.max(math.abs(self.frontVelocity) - self.acceleration * dt, 0) end
@@ -35,12 +67,30 @@ function Player:update(dt)
     else self.sideVelocity = math.sign(self.sideVelocity) * math.max(math.abs(self.sideVelocity) - self.acceleration * dt, 0) end
 
     if input:down('lclick') then self:shoot() end
+
+
+    --Stats
+    self.stamina = math.min(self.stamina + self.staminaDecrase * dt, self.maxStamina)
+    self.hp = math.min(self.hp + self.hpDecrase * dt, self.maxHP)
+    self.power = self.minPower + (self.maxPower - self.minPower) * self.hp/self.maxHP
     
     self.collider:setLinearVelocity(self.sideVelocity, -self.frontVelocity)
 end
 
 function Player:draw()
-    love.graphics.circle('line', self.x, self.y, self.r)
+    pushRotate(self.x, self.y, self.angle)
+    for i, polygon in ipairs(self.polygons) do
+        local points = fn.map(polygon, function(k, v) 
+        	if k % 2 == 1 then 
+          		return self.x + v + random(-0.5, 0.5) 
+        	else 
+          		return self.y + v + random(-0.5, 0.5) 
+        	end 
+        end)
+        love.graphics.setColor(self.polygonColors[i])
+        love.graphics.polygon('line', points)
+    end
+    love.graphics.pop()
 
     --Debug
     --love.graphics.line(self.x, self.y, self.x + 2 * self.r * math.cos(self.angle), self.y + 2 * self.r * math.sin(self.angle))
@@ -50,10 +100,10 @@ end
 
 function Player:shoot()
     effectPos = function(obj)
-        return obj.x + 1.1 * obj.r * math.cos(obj.angle), obj.y + 1.1 * obj.r * math.sin(obj.angle)
+        return obj.x + 1.1 * obj.width/2 * math.cos(obj.angle), obj.y + 1.1 * obj.width/2 * math.sin(obj.angle)
     end
 
     self.area:AddGameObject('ShootEffect', select(1, effectPos(self)), select(2, effectPos(self)), {parent = self, getPos = effectPos})
 
-    self.area:AddGameObject('Projectile', self.x + 1.5 * self.r * math.cos(self.angle), self.y + 1.5 * self.r * math.sin(self.angle), {r = self.angle})
+    self.area:AddGameObject('Projectile', self.x + 1.5 * self.width/2 * math.cos(self.angle), self.y + 1.5 * self.width/2* math.sin(self.angle), {r = self.angle})
 end
